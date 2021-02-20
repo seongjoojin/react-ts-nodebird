94f8fa13a996bd78e23048824ea30c151ffdc34c
Seongjoo Jin <evanjin8@gmail.com>
2021-02-20 23:10:08 +0900

    Link와 eslint

--- front/components/AppLayout.tsx
+++ front/components/AppLayout.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from 'react';
+import Link from 'next/link';
 
 type Props = {
   children: ReactNode;
@@ -7,10 +8,20 @@ type Props = {
 const AppLayout: FC<Props> = ({ children }) => {
   return (
     <div>
-      <div>공통메뉴</div>
+      <div>
+        <Link href="/">
+          <a>노드버드</a>
+        </Link>
+        <Link href="/profile">
+          <a>프로필</a>
+        </Link>
+        <Link href="/signup">
+          <a>회원가입</a>
+        </Link>
+      </div>
       {children}
     </div>
-  )
+  );
 };
 
 export default AppLayout;
