b159bc54026ba62ddd3706e610232f3b4a938653
EvanJin <evanjin@kakao.com>
2021-03-06 23:16:34 +0900

    익스프레스 라우터 분리하기

--- back/app.ts
+++ back/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import postRouter from './routes/post';
 
 const app = express();
 
@@ -6,27 +7,7 @@ app.get('/', (req, res) => {
   res.send('hello express')
 })
 
-app.get('/api', (req, res) => {
-  res.send('hello api');
-});
-
-app.get('/api/posts', (req, res) => {
-  res.json([
-    { id: 1, content: 'hello' },
-    { id: 2, content: 'hello' },
-    { id: 3, content: 'hello' },
-  ]);
-});
-
-app.post('/api/post', (req, res) => {
-
-});
-
-app.delete('/api/posts', (req, res) => {
-
-});
-
-
+app.use('/post',postRouter);
 
 app.listen(3065, () => {
   console.log('서버 실행');
